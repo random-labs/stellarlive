2df344bc0e0c81c6689ec8902b5fcbea2a2ea17c
Derek Tor <derekmuoktor@gmail.com>
2014-11-16 03:11:05 -0800

    working line graph

--- js/main.js
+++ js/main.js
@@ -4,33 +4,44 @@ app.controller("MainCtrl", function($scope, $firebase, $interval) {
   var ref = new Firebase("https://stellarlive.firebaseio.com/");
   var sync = $firebase(ref);
   // create a synchronized array for use in our HTML code
-  transactions = sync.$asArray();
+  var transactions = sync.$asArray();
+
 
   $interval(function() {
 
     transactions.$loaded().then(function() {
-      $scope.plotData = [{"key":"Series 1",
+      $scope.transactions = transactions.slice(-10);
+      $scope.plotData = [
+                          {
+                            "key":"Series 1",
                              "values":[]
-                         }]
+                          },
+                          {
+                            "key":"Series 2",
+                             "values":[]
+                          }
+
+                         ]
 
        for (var i = 0; i < transactions.length; i++) {
-         var unixtime = new Date().getTime() / 1000
-         var transaction = [ unixtime ,transactions[i].transactionAmount/1000000]
+         var transaction = [ transactions[i].timestamp ,transactions[i].transactionAmount/1000000]
+         var fee = [ transactions[i].timestamp ,transactions[i].transactionFee]
         //  {
         //                     "x":transactions[i].transactionAmount/1000000,
         //                     "y":transactions[i].transactionFee,
         //                     "size":1
         //                   }
-          // if($scope.plotData[0].values.length >=100){
-          //     $scope.plotData[0].values.push(transaction);
-          //     $scope.plotData[0].values = $scope.plotData[0].values.slice(-100);
-          // }else{
-          //   $scope.plotData[0].values.push(transaction);
-          // }
+          if($scope.plotData[0].values.length >=10){
+              $scope.plotData[0].values.push(transaction);
+              $scope.plotData[0].values = $scope.plotData[0].values.slice(-10);
+          }else{
+            $scope.plotData[0].values.push(transaction);
+            // $scope.plotData[1].values.push(fee);
+          }
        }
     });
 
-  }, 1000);
+  }, 100/10);
 
 
 
